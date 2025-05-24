39efc50ffb9dc4f5f6fe02ef1674a6db080baa45
NextGenDataLead <nextgendatalead@gmail.com>
2025-05-24 20:24:50 +0200

    Test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d1b0234596778e09"; }
+/******/ 		__webpack_require__.h = function() { return "c2f215513bd88d57"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import { Card } from "@/components/ui/card"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { services } from "@/config/services"
-import Link from "next/link"
-import { ArrowRight } from "lucide-react"
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { services } from "@/config/services";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function ServicesPage() {
   return (
